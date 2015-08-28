72a7bae3fe71bfae439b7653bccc4fc9a452e63c
Michael Dykman <mdykman@gmail.com>
2015-08-28 01:20:43 -0400

    fixed alias thing in jdbc

--- src/main/java/org/dykman/jtl/modules/JdbcModule.java
+++ src/main/java/org/dykman/jtl/modules/JdbcModule.java
@@ -38,168 +38,144 @@ import com.google.common.util.concurrent.ListeningExecutorService;
 
 public class JdbcModule implements Module {
 
-	final JSONObject baseConfig;
+   final JSONObject baseConfig;
 
-	public JdbcModule(JSONObject config) {
-		baseConfig = config;
-	}
+   public JdbcModule(JSONObject config) {
+      baseConfig = config;
+   }
 
-	interface Executor {
-		JSON process(PreparedStatement stat, JSONBuilder builder)
-				throws SQLException;
-	}
+   interface Executor {
+      JSON process(PreparedStatement stat, JSONBuilder builder) throws SQLException;
+   }
 
-	static class JdbcConnectionWrapper {
-		JSONObject conf;
+   static class JdbcConnectionWrapper {
+      JSONObject conf;
 
-		JdbcConnectionWrapper(JSONObject conf) {
-			this.conf = conf;
-		}
+      JdbcConnectionWrapper(JSONObject conf) {
+         this.conf = conf;
+      }
 
-		Connection connection = null;
+      Connection connection = null;
 
-		public Connection getConnection(SourceInfo src) throws ExecutionException {
-			if (connection == null) {
-				synchronized (JdbcModule.class) {
-					if (connection == null) {
-						String driver = stringValue(conf.get("driver"));
-						String uri = stringValue(conf.get("uri"));
-						String user = stringValue(conf.get("user"));
-						String password = stringValue(conf.get("password"));
-						if (driver != null) {
-							try {
-								Class<Driver> drc = (Class<Driver>) Class
-										.forName(driver);
-								Driver drv = drc.newInstance();
-								Properties properties = new Properties();
-								if (user != null) {
-									properties.setProperty("user", user);
-									properties
-											.setProperty("password", password);
-									// connection = drv.connect(uri,properties);
-									connection = DriverManager.getConnection(
-											uri, properties);
-								} else {
-									connection = DriverManager
-											.getConnection(uri);
-								}
-							} catch (ClassNotFoundException
-									| InstantiationException
-									| IllegalAccessException e) {
-								throw new ExecutionException(
-										"JDBC: unable to load class " + driver,src);
-							} catch (SQLException e) {
-                        throw new ExecutionException(
-										"JDBC: unable to connect to " + uri,src);
-							}
-						}
-					}
-				}
-			}
-			return connection;
-		}
+      public Connection getConnection(SourceInfo src) throws ExecutionException {
+         if(connection == null) {
+            synchronized(JdbcModule.class) {
+               if(connection == null) {
+                  String driver = stringValue(conf.get("driver"));
+                  String uri = stringValue(conf.get("uri"));
+                  String user = stringValue(conf.get("user"));
+                  String password = stringValue(conf.get("password"));
+                  if(driver != null) {
+                     try {
+                        Class<Driver> drc = (Class<Driver>) Class.forName(driver);
+                        Driver drv = drc.newInstance();
+                        Properties properties = new Properties();
+                        if(user != null) {
+                           properties.setProperty("user", user);
+                           properties.setProperty("password", password);
+                           // connection = drv.connect(uri,properties);
+                           connection = DriverManager.getConnection(uri, properties);
+                        } else {
+                           connection = DriverManager.getConnection(uri);
+                        }
+                     } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                        throw new ExecutionException("JDBC: unable to load class " + driver, src);
+                     } catch (SQLException e) {
+                        throw new ExecutionException("JDBC: unable to connect to " + uri, src);
+                     }
+                  }
+               }
+            }
+         }
+         return connection;
+      }
 
-		public InstructionFuture<JSON> query(SourceInfo meta,Executor exec) {
-			// Connection c = getConnection();
-			return new AbstractInstructionFuture(meta) {
-				@Override
-				public ListenableFuture<JSON> _call(
-						final AsyncExecutionContext<JSON> context,
-						final ListenableFuture<JSON> data)
-						throws ExecutionException {
-					InstructionFuture<JSON> q = context.getdef("1");
-					InstructionFuture<JSON> p = context.getdef("2");
-					List<ListenableFuture<JSON>> ll = new ArrayList<>();
-					ll.add(q.call(context, data));
-					if (p != null) {
-						ll.add(p.call(context, data));
-					}
-					return transform(allAsList(ll),
-							new AsyncFunction<List<JSON>, JSON>() {
-								@Override
-								public ListenableFuture<JSON> apply(
-										List<JSON> input) throws Exception {
-									Iterator<JSON> jit = input.iterator();
-									final JSON qq = jit.next();
-									if(! qq.isValue()) return Futures.immediateFailedCheckedFuture(
-									      new ExecutionException("query is not a string: " + qq.toString(),meta));
-									final JSON pp = jit.hasNext() ? jit.next()
-											: null;
-//                           if(pp !=null && ! (pp instanceof JSONArray)) return Futures.immediateFailedCheckedFuture(
-//                                 new ExecutionException("parameters are not an array: " + pp.toString(),meta));
-									Callable<JSON> cc = new Callable<JSON>() {
-										@Override
-										public JSON call() throws Exception {
-											Connection connection = getConnection(source);
-											PreparedStatement prep = connection
-													.prepareStatement(stringValue(qq));
-											System.err.print("query:" + stringValue(qq));
-											if(pp!=null) System.err.println(" " + pp.toString());
-											System.err.println();
-											
-											if (pp != null) {
-												switch (pp.getType()) {
-                                    case FRAME:
-												case ARRAY:
-													JSONArray arr = (JSONArray) pp;
-													int i = 1;
-													for (JSON j : arr) {
-														if (!j.isValue())
-															throw new ExecutionException(
-																	"query parameter "
-																			+ i
-																			+ " is not a scalar value: "
-																			+ j.toString(),meta);
-														
-														prep.setObject(i++,
-																((JSONValue) j)
-																		.get());
-													}
-													break;
-												case NULL:
-												case STRING:
-												case LONG:
-												case BOOLEAN:
-												case DOUBLE:
-													prep.setObject(1,
-															((JSONValue) pp)
-																	.get());
-													break;
-												default:
-													throw new ExecutionException(
-															"query single parameter is not a scalar value:"
-																	+ pp.toString(),meta);
-												}
+      public InstructionFuture<JSON> query(SourceInfo meta, Executor exec) {
+         // Connection c = getConnection();
+         return new AbstractInstructionFuture(meta) {
+            @Override
+            public ListenableFuture<JSON> _call(final AsyncExecutionContext<JSON> context,
+                  final ListenableFuture<JSON> data) throws ExecutionException {
+               InstructionFuture<JSON> q = context.getdef("1");
+               InstructionFuture<JSON> p = context.getdef("2");
+               List<ListenableFuture<JSON>> ll = new ArrayList<>();
+               ll.add(q.call(context, data));
+               if(p != null) {
+                  ll.add(p.call(context, data));
+               }
+               return transform(allAsList(ll), new AsyncFunction<List<JSON>, JSON>() {
+                  @Override
+                  public ListenableFuture<JSON> apply(List<JSON> input) throws Exception {
+                     Iterator<JSON> jit = input.iterator();
+                     final JSON qq = jit.next();
+                     if(!qq.isValue())
+                        return Futures.immediateFailedCheckedFuture(new ExecutionException("query is not a string: "
+                              + qq.toString(), meta));
+                     final JSON pp = jit.hasNext() ? jit.next() : null;
+                     // if(pp !=null && ! (pp instanceof JSONArray)) return
+                     // Futures.immediateFailedCheckedFuture(
+                     // new ExecutionException("parameters are not an array: " +
+                     // pp.toString(),meta));
+                     Callable<JSON> cc = new Callable<JSON>() {
+                        @Override
+                        public JSON call() throws Exception {
+                           Connection connection = getConnection(source);
+                           PreparedStatement prep = connection.prepareStatement(stringValue(qq));
+                           System.err.print("query:" + stringValue(qq));
+                           if(pp != null)
+                              System.err.println(" " + pp.toString());
+                           System.err.println();
 
-											}
-											
-											return exec.process(prep,
-													context.builder());
+                           if(pp != null) {
+                              switch(pp.getType()) {
+                              case FRAME:
+                              case ARRAY:
+                                 JSONArray arr = (JSONArray) pp;
+                                 int i = 1;
+                                 for(JSON j : arr) {
+                                    if(!j.isValue())
+                                       throw new ExecutionException("query parameter " + i + " is not a scalar value: "
+                                             + j.toString(), source);
 
-										}
-									};
-									return context.executor().submit(cc);
+                                    prep.setObject(i++, ((JSONValue) j).get());
+                                 }
+                                 break;
+                              case NULL:
+                              case STRING:
+                              case LONG:
+                              case BOOLEAN:
+                              case DOUBLE:
+                                 prep.setObject(1, ((JSONValue) pp).get());
+                                 break;
+                              default:
+                                 throw new ExecutionException(
+                                       "single parameter is not a scalar value:" + pp.toString(), source);
+                              }
 
-								}
-							});
-				}
-			};
-		}
-	}
+                           }
+                           return exec.process(prep, context.builder());
+                        }
+                     };
+                     return context.executor().submit(cc);
 
-	Executor queryExecutor = new Executor() {
+                  }
+               });
+            }
+         };
+      }
+   }
+
+   Executor queryExecutor = new Executor() {
       @Override
-      public JSON process(PreparedStatement stat, JSONBuilder builder)
-            throws SQLException {
+      public JSON process(PreparedStatement stat, JSONBuilder builder) throws SQLException {
          ResultSet rs = stat.executeQuery();
          Frame frame = builder.frame();
          ResultSetMetaData rsm = rs.getMetaData();
          int n = rsm.getColumnCount();
-         while (rs.next()) {
+         while(rs.next()) {
             JSONObject obj = builder.object(frame);
-            for (int i = 1; i <= n; ++i) {
-               obj.put(rsm.getColumnName(i),
-                     builder.value(rs.getObject(i)));
+            for(int i = 1; i <= n; ++i) {
+               obj.put(rsm.getColumnLabel(i), builder.value(rs.getObject(i)));
             }
             frame.add(obj);
          }
@@ -208,10 +184,9 @@ public class JdbcModule implements Module {
    };
    Executor insertExecutor = new Executor() {
       @Override
-      public JSON process(PreparedStatement stat, JSONBuilder builder)
-            throws SQLException {
+      public JSON process(PreparedStatement stat, JSONBuilder builder) throws SQLException {
          stat.executeUpdate();
-//         stat.execute();
+         // stat.execute();
          PreparedStatement lid = stat.getConnection().prepareStatement("select last_insert_id()");
          ResultSet rs = lid.executeQuery();
          JSON r = builder.value();
@@ -220,69 +195,74 @@ public class JdbcModule implements Module {
          }
          stat.close();
          lid.close();
-         
+
          return r;
       }
    };
-	@Override
-	public void define(SourceInfo meta,AsyncExecutionContext<JSON> context) {
-		JdbcConnectionWrapper wrapper = new JdbcConnectionWrapper(baseConfig);
-		SourceInfo si = meta.clone();
-		si.name = "query";
-		context.define("query", wrapper.query(si,queryExecutor));
+
+   @Override
+   public void define(SourceInfo meta, AsyncExecutionContext<JSON> context) {
+      JdbcConnectionWrapper wrapper = new JdbcConnectionWrapper(baseConfig);
+      SourceInfo si = meta.clone();
+      si.name = "query";
+      si.code = "*internal*";
+      context.define("query", wrapper.query(si, queryExecutor));
 
       si = meta.clone();
       si.name = "cquery";
-	    context.define("cquery", wrapper.query(si,new Executor() {
-	         @Override
-	         public JSON process(PreparedStatement stat, JSONBuilder builder)
-	               throws SQLException {
-	            ResultSet rs = stat.executeQuery();
-	            ResultSetMetaData rsm = rs.getMetaData();
-	            int n = rsm.getColumnCount();
-               JSONObject obj = builder.object(null);
-               JSONArray[] aar = new JSONArray[n];
-               for(int i  = 1;i <= n;++i) {
-                  JSONArray arr = builder.array(obj);
-                  aar[i-1] = arr;
-                  obj.put(rsm.getColumnName(i),arr);
+      si.code = "*internal*";
+      context.define("cquery", wrapper.query(si, new Executor() {
+         @Override
+         public JSON process(PreparedStatement stat, JSONBuilder builder) throws SQLException {
+            ResultSet rs = stat.executeQuery();
+            ResultSetMetaData rsm = rs.getMetaData();
+            int n = rsm.getColumnCount();
+            JSONObject obj = builder.object(null);
+            JSONArray[] aar = new JSONArray[n];
+            for(int i = 1; i <= n; ++i) {
+               JSONArray arr = builder.array(obj);
+               aar[i - 1] = arr;
+               obj.put(rsm.getColumnLabel(i), arr);
+            }
+            while(rs.next()) {
+               for(int i = 1; i <= n; ++i) {
+                  aar[i - 1].add(builder.value(rs.getObject(i)));
                }
-	            while (rs.next()) {
-	               for (int i = 1; i <= n; ++i) {
-	                  aar[i-1].add(builder.value(rs.getObject(i)));
-	               }
-	            }
-	            return obj;
-	         }
-	      }));
+            }
+            return obj;
+         }
+      }));
 
-	   si = meta.clone();
-	   si.name = "execute";
+      si = meta.clone();
+      si.code = "*internal*";
+      si.name = "execute";
 
-		context.define("execute", wrapper.query(si,new Executor() {
-			@Override
-			public JSON process(PreparedStatement stat, JSONBuilder builder)
-					throws SQLException {
-			   stat.execute();
-				return builder.value(true);
-			}
+      si = meta.clone();
+      si.name = "insert";
+      si.code = "*internal*";
+      context.define("execute", wrapper.query(si, new Executor() {
+         @Override
+         public JSON process(PreparedStatement stat, JSONBuilder builder) throws SQLException {
+            stat.execute();
+            return builder.value(true);
+         }
 
-		}));
+      }));
 
-	    context.define("insert", wrapper.query(si,insertExecutor));
-	}
+      context.define("insert", wrapper.query(si, insertExecutor));
+   }
 
-	protected static String stringValue(JSON j) {
-		if (j == null)
-			return null;
-		switch (j.getType()) {
-		case STRING:
-		case DOUBLE:
-		case LONG:
-			return ((JSONValue) j).stringValue();
-		default:
-			return null;
-		}
-	}
+   protected static String stringValue(JSON j) {
+      if(j == null)
+         return null;
+      switch(j.getType()) {
+      case STRING:
+      case DOUBLE:
+      case LONG:
+         return ((JSONValue) j).stringValue();
+      default:
+         return null;
+      }
+   }
 
 }
